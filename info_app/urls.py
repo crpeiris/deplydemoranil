a495bc6b12cd0508900de8cc6525da7b2acbb2db
Ranil Peiris <cranil@sjp.ac.lk>
2025-10-29 09:56:39 +0530

    welcome page

--- info_app/urls.py
+++ info_app/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('info/', include('info_app.urls')),
-    
+    path('', views.welcome , name='welcome' ),
 ]
