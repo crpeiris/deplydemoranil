6fd238f25b37ed82f30df96beec68cf68a0d3b20
Shakindi Kithmini <shakindikithmini@gmail.com>
2025-10-29 11:22:30 +0530

    initial commit

--- info_app/urls.py
+++ info_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome , name='welcome' ),
+    path('projects/', views.projects , name='projects' ),
 ]
--- info_app/views.py
+++ info_app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def welcome(request):
     return render(request, 'info_app/welcome.html')
+
+
+def projects(request):
+    """Render the projects page."""
+    return render(request, 'info_app/projects.html')
