f8b31b264dec593347a55b48ddf9c9ca81f70351
Ranil Peiris <cranil@sjp.ac.lk>
2025-10-29 09:23:05 +0530

    home page

--- deploydemo2025/urls.py
+++ deploydemo2025/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from info_app.views import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('info/', include('info_app.urls')),
+    path('', views.welcome, name='welcome' ),
+    
 ]
