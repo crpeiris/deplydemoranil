d97518f9c8194526a138fa6fee48f2b7d2f5ec97
Binura <binurakoralage02@gmail.com>
2025-10-29 11:20:26 +0530

    Updated Support

--- info_app/views.py
+++ info_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def welcome(request):
     return render(request, 'info_app/welcome.html')
+
+def support(request):
+    return render(request, 'info_app/support.html')
