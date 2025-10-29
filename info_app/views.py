895434517665e5996082ed5a1dacc3b654727754
shashaa <shashadya.644@gmail.com>
2025-10-29 10:55:15 +0530

    updated resources

--- info_app/views.py
+++ info_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def welcome(request):
     return render(request, 'info_app/welcome.html')
+
+
+def resources(request):
+    return render(request, 'info_app/resources.html')
