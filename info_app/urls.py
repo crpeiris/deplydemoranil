25b0f43ddec0ae2a7bf26d3b726da4779f2cd757
thishakyadarmawardhana <thishakyadarmawardhana@gmail.com.com>
2025-10-29 12:11:14 +0530

    Your commit message describing the changes

--- info_app/urls.py
+++ info_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome , name='welcome' ),
+    path('project/', views.project , name='project' ),
+
 ]
--- info_app/views.py
+++ info_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def welcome(request):
     return render(request, 'info_app/welcome.html')
+
+def project(request):
+    return render(request, 'info_app/project.html')
