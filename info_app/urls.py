cbcb6060419c8f538b152cb3cfaf6aaa8cb8a5e8
Imali <imalishehara@gmail.com>
2025-10-29 12:08:36 +0530

    add the lab lab_sessions

--- info_app/urls.py
+++ info_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome , name='welcome' ),
+    path('lab_sessions.html', views.lab_sessions , name='lab_sessions' ),
 ]
--- info_app/views.py
+++ info_app/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def welcome(request):
     return render(request, 'info_app/welcome.html')
+def lab_sessions(request):
+    return render(request, 'info_app/lab_sessions.html')
